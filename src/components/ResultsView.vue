40e1c8bbfb37f970e0c80ee1765d809255123e65
Jacob Henry <jchenry@mitre.org>
2019-08-13 19:38:50 -0400

    Fixed card heights
    Signed-off-by: Jacob Henry <jchenry@mitre.org>

--- src/components/ResultsView.vue
+++ src/components/ResultsView.vue
@@ -6,22 +6,28 @@
     <!-- Compliance Cards -->
     <v-row justify="space-around">
       <v-col xs-4>
-        <v-card>
+        <v-card class="fill-height">
           <v-card-title>Status Counts</v-card-title>
-          <v-card-text><StatusChart :filter="{}"/></v-card-text>
+          <v-card-text
+            ><StatusChart :filter="filter" v-on:filter-status="setStatusFilter"
+          /></v-card-text>
         </v-card>
       </v-col>
       <v-col xs-4>
-        <v-card>
+        <v-card class="fill-height">
           <v-card-title>Severity Counts</v-card-title>
-          <v-card-text><SeverityChart :filter="{}"/></v-card-text>
+          <v-card-text
+            ><SeverityChart
+              :filter="filter"
+              v-on:filter-severity="setSeverityFilter"
+          /></v-card-text>
         </v-card>
       </v-col>
       <v-col xs-4>
-        <v-card>
-          <v-card-title class="fill-height">Compliance Level</v-card-title>
+        <v-card class="fill-height">
+          <v-card-title>Compliance Level</v-card-title>
           <v-card-text>
-            <ComplianceChart :filter="{}" />
+            <ComplianceChart :filter="filter" />
             [Passed/(Passed + Failed + Not Reviewed + Profile Error) * 100]
           </v-card-text>
         </v-card>
@@ -29,15 +35,24 @@
     </v-row>
 
     <!-- TreeMap and Partition Map -->
-    <v-sheet class="my-4 px-4" elevation="2" title="test">
-      <h2>TreeMap and Others</h2>
-    </v-sheet>
+    <v-row>
+      <v-col xs-12>
+        <v-card elevation="2" title="test">
+          <v-card-title>TreeMap </v-card-title>
+          <v-card-text> WIP </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
     <!-- DataTable -->
-    <v-sheet class="my-4 px-4" elevation="2">
-      <h2>Results View Data</h2>
-      <ControlTable />
-    </v-sheet>
+    <v-row>
+      <v-col xs-12>
+        <v-sheet class="my-4 px-4" elevation="2">
+          <h2>Results View Data</h2>
+          <ControlTable />
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -76,21 +91,35 @@ export default class Results extends ResultsProps {
   setSeverityFilter(newSeverity: Severity): void {
     // If they've picked the same one, we reset to undefined.
     // Otherwise, we set it as the new filter
+    let newFilter = { ...this.filter };
     if (newSeverity === this.filter.severity) {
-      this.filter.severity = undefined;
+      newFilter.severity = undefined;
     } else {
-      this.filter.severity = newSeverity;
+      newFilter.severity = newSeverity;
     }
+    this.filter = newFilter;
   }
 
   setStatusFilter(newStatus: ControlStatus): void {
     // If they've picked the same one, we reset to undefined.
     // Otherwise, we set it as the new filter
+    let newFilter = { ...this.filter };
     if (newStatus === this.filter.status) {
-      this.filter.status = undefined;
+      newFilter.status = undefined;
     } else {
-      this.filter.status = newStatus;
+      newFilter.status = newStatus;
     }
+    this.filter = newFilter;
   }
 }
 </script>
+
+<!--
+<style scoped>
+.card {
+  overflow: hidden;
+  flex-grow: 1;
+  height: 107%; /*ehhh*/
+}
+</style>
+-->
