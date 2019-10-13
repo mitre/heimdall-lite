7e52f046968de0b9937ba6f1eb664f10d81762ea
Jacob Henry <jchenry@mitre.org>
2019-10-13 14:04:23 -0400

    Fix issue #78
    Signed-off-by: Jacob Henry <jchenry@mitre.org>

--- src/store/app_info.ts
+++ src/store/app_info.ts
@@ -4,6 +4,9 @@ import Store from "@/store/store";
 declare const process: {
   env: {
     PACKAGE_VERSION: string;
+    DESCRIPTION: string;
+    REPOSITORY: string;
+    LICENSE: string;
   };
 };
 
@@ -16,6 +19,15 @@ declare const process: {
 class AppInfoModule extends VuexModule {
   /** The app version */
   version: string = `Ver.${process.env.PACKAGE_VERSION}`;
+
+  /** The app description */
+  description: string = process.env.DESCRIPTION;
+
+  /** The app repository */
+  repository: string = process.env.REPOSITORY;
+
+  /** The app license */
+  license: string = process.env.LICENSE;
 }
 
 export default AppInfoModule;
