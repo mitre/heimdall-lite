d39253ac368592f7a58add840b00a4ab43db6b3f
Jacob Henry <jchenry@mitre.org>
2019-10-14 12:27:21 -0400

    Export CAAT now respects filters.
    Also, better formatting on the result column (to not say PASSED when it's a fail, etc.)
    
    Fixes #101
    
    Signed-off-by: Jacob Henry <jchenry@mitre.org>

--- src/components/global/Sidebar.vue
+++ src/components/global/Sidebar.vue
@@ -12,7 +12,7 @@
 
     <v-list dense class="px-2" subheader>
       <v-subheader>Tools</v-subheader>
-      <ExportCaat></ExportCaat>
+      <slot></slot>
     </v-list>
 
     <v-list dense class="px-2" subheader>
@@ -37,7 +37,6 @@ import { getModule } from "vuex-module-decorators";
 import InspecDataModule from "@/store/data_store";
 import FileItem from "@/components/global/sidebaritems/SidebarFile.vue";
 import LinkItem from "@/components/global/sidebaritems/SidebarLink.vue";
-import ExportCaat from "@/components/global/ExportCaat.vue";
 import AboutModal from "@/components/global/AboutModal.vue";
 import HelpModal from "@/components/global/HelpModal.vue";
 
@@ -52,7 +51,6 @@ const SidebarProps = Vue.extend({
   components: {
     LinkItem,
     FileItem,
-    ExportCaat,
     AboutModal,
     HelpModal
   }
