7a6ae4baeb7817262b019b80b91f2924b5fd57e8
Colin Doherty <oneofthree103@gmail.com>
2020-06-23 11:31:19 -0400

    Moved samples to assets

--- src/components/global/upload_tabs/SampleList.vue
+++ src/components/global/upload_tabs/SampleList.vue
@@ -30,10 +30,21 @@ import InspecIntakeModule, {
 } from "@/store/report_intake";
 import InspecDataModule from "../../../store/data_store";
 import AppInfoModule from "../../../store/app_info";
+import aws_s3_baseline from "../../../assets/samples/aws-s3-baseline.json";
+import bad_nginx from "../../../assets/samples/bad_nginx.json";
+import cis_aws_foundations_baseline from "../../../assets/samples/cis-aws-foundations-baseline.json";
+import fortify_h_tools_conv_webgoat from "../../../assets/samples/fortify_h_tools_conv_webgoat.json";
+import good_nginxresults from "../../../assets/samples/good_nginxresults.json";
+import owasp_zap_webgoat from "../../../assets/samples/owasp_zap_webgoat.json";
+import owasp_zap_zero from "../../../assets/samples/owasp_zap_zero.webappsecurity.json";
+import rhel_cve_vulnerability_scan_baseline_with_failures from "../../../assets/samples/rhel_cve_vulnerability_scan_baseline_with_failures.json";
+import rhel7_results from "../../../assets/samples/rhel7-results.json";
+import sonarqube_java_sample from "../../../assets/samples/sonarqube_java_sample.json";
+import ubuntu_1604_baseline_results from "../../../assets/samples/ubuntu-16.04-baseline-results.json";
 
 interface Sample {
   name: string;
-  url: string;
+  sample: any;
 }
 
 // We declare the props separately to make props types inferable.
@@ -53,48 +64,48 @@ export default class SampleList extends Props {
     return [
       {
         name: "Sonarqube Java Heimdall_tools Sample",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/sonarqube_java_sample.json`
+        sample: sonarqube_java_sample
       },
       {
         name: "OWASP ZAP Webgoat Heimdall_tools Sample",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/owasp_zap_webgoat.json`
+        sample: owasp_zap_webgoat
       },
       {
         name: "OWASP ZAP Zero_WebAppSecurity Heimdall_tools Sample",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/owasp_zap_zero.webappsecurity.json`
+        sample: owasp_zap_zero
       },
       {
         name: "Fortify Heimdall_tools Sample",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/fortify_h_tools_conv_webgoat.json`
+        sample: fortify_h_tools_conv_webgoat
       },
       {
         name: "AWS S3 Permissions Check",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/aws-s3-baseline.json`
+        sample: aws_s3_baseline
       },
       {
         name: "AWS CIS Foundations Baseline",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/cis-aws-foundations-baseline.json`
+        sample: cis_aws_foundations_baseline
       },
       {
         name: "NGINX Clean Sample",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/good_nginxresults.json`
+        sample: good_nginxresults
       },
       {
         name: "NGINX With Failing Tests",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/bad_nginx.json`
+        sample: bad_nginx
       },
       {
         name: "Red Hat CVE Vulnerability Scan",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/rhel_cve_vulnerability_scan_baseline_with_failures.json`
+        sample: rhel_cve_vulnerability_scan_baseline_with_failures
       },
       {
         name: "RedHat 7 STIG Baseline",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/rhel7-results.json`
-      },
-      {
-        name: "Ubuntu STIG Baseline",
-        url: `https://raw.githubusercontent.com/${this.repo}/master/samples/ubuntu-16.04-baseline-results.json`
+        sample: rhel7_results
       }
+      /*{
+        name: "Ubuntu STIG Baseline",
+        sample: ubuntu_1604_baseline_results
+      }*/
     ];
   }
 
@@ -105,32 +116,25 @@ export default class SampleList extends Props {
 
   /** Callback for our list item clicks */
   load_sample(sample: Sample) {
-    // Generate an id
-    let unique_id = next_free_file_ID();
-
-    // Get intake module
+    console.log(sample.sample);
     let intake_module = getModule(InspecIntakeModule, this.$store);
-    // Do fetch
-    fetch(sample.url, { method: "get" })
-      .then(response => response.text())
-      .then(text =>
-        intake_module.loadText({ filename: sample.name, unique_id, text })
-      )
+    let unique_id = next_free_file_ID();
+    return intake_module
+      .loadText({
+        text: JSON.stringify(sample.sample),
+        filename: sample.name,
+        unique_id
+      })
       .then(err => {
-        // Handle errors if necessary
         if (err) {
-          throw `Error loading sample ${sample.name}`;
+          console.error(`Error loading sample ${sample.name}`);
+          this.$toasted.global.error({
+            message: String(err),
+            isDark: this.$vuetify.theme.dark
+          });
         } else {
-          // Emit success
           this.$emit("got-files", [unique_id]);
         }
-      })
-      .catch(err => {
-        // Toast whatever error we got
-        this.$toasted.global.error({
-          message: String(err),
-          isDark: this.$vuetify.theme.dark
-        });
       });
   }
 }
